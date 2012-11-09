88aa0703ca8e0069cec46dc51286f6d04f52e9b6
whitperson <whitperson@gmail.com>
2012-11-09 11:57:44 -0500

    quiz 9 complete

--- quizzes/quiz9.rb
+++ quizzes/quiz9.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
-class Mathy
-	def factorial(n)
+class Mathematics
+	def self.factorial(n)
 		n = (1..n).inject(:*) 
 	end
 end	
 
-m1 = Mathy.new
+m1 = Mathematics.new
 
 puts "on which number do you want to run the factorial function?"
 m1 = gets.chomp.to_f
 
-result = Mathy.factorial(m1)
-
+result = Mathematics.factorial(m1)
+puts "#{result}"
