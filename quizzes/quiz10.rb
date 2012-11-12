62fdbd852a0a171b8afbc726b693609a77e72a9a
whitperson <whitperson@gmail.com>
2012-11-12 10:44:13 -0500

    fix

--- quizzes/quiz10.rb
+++ quizzes/quiz10.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 puts "type in a (n)umber"
 response = gets.chomp.to_i
-response.explode
 
-     begin
-          response == 10
-          puts "the square of __10__ is 100"
-     rescue
+begin
+	response.explode
+rescue
+
 end
+   puts "the square of #{response} is #{response ** 2}"
